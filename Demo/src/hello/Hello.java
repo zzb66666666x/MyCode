18833dde75bdca6b3d29b528f75ef37ecbd008ea
zzb66666666x <18767066589@163.com>
2020-06-02 15:05:23 +0800

    first trial in java

---
+++ Demo/src/hello/Hello.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("I am in the main of Hello");
+    }
+    public static int value = 10;
+}
---
+++ Demo/src/test.java
@@ -0,0 +1,7 @@
+import hello.Hello;
+
+public class test {
+    public static void main(String[] args){
+        Hello.main(args);
+    }
+}
