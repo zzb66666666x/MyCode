67c502e64f3e50990dc596ac8d6fba73d738896a
zzb66666666x <18767066589@163.com>
2020-06-02 20:22:25 +0800

    updated

---
+++ Demo/src/formatting_print/test_print.java
@@ -0,0 +1,36 @@
+package formatting_print;
+
+public class test_print {
+    public static void main(String[] args)
+	{
+		String name = "盖伦";
+		int kill = 8;
+		String title = "超神";
+		
+		String sentence = name + " 在进行了连续  " + kill + " 次击杀后，获得了 " + title + " 的称号 ";
+		//直接使用+进行字符串连接，编码感觉会比较繁琐，并且维护性差，易读性差
+		System.out.println(sentence);
+		
+		String sentenceFormat = "%s 在进行了连续 %d 次击杀后， 获得了 %s 的称号%n";
+		//格式化输出，%s表示字符串，%d表示数字，%n表示换号
+		System.out.printf(sentenceFormat, name, kill, title);
+		System.out.format(sentenceFormat, name, kill, title);
+		//format和printf能够达到一模一样的效果
+		
+		int year = 2020; 
+		
+		System.out.format("%d%n",year);		//用%n或\n没差
+		//直接打印数字
+		System.out.printf("%8d%n",year);	//用printf还是format没差
+		//总长度为8，默认右对齐
+		System.out.printf("%-8d%n",year);
+		//总长度为8，默认左对齐
+		System.out.printf("%08d%n",year);
+		//总长度为8，不够补0
+		System.out.printf("%,8d%n",year*10000);
+		//千位分隔符
+		System.out.format("%.2f%n",Math.PI);
+		//小数点位数
+        System.out.println("end of testing the formatting print");
+    }
+}
--- Demo/src/test.java
+++ Demo/src/test.java
@@ -1,5 +1,6 @@
 import hello.Hello;
-import java.util.Scanner;   
+import java.util.Scanner;  
+import formatting_print.test_print; 
 /*
 Java的实用工具类库java.util包。
 在这个包中，Java提供了一些实用的方法和数据结构。
@@ -10,23 +11,83 @@ Java的实用工具类库java.util包。
 
 public class test {
     public static void main(String[] args){
-        Hello.main(args);
-        //testing input
         Scanner in = new Scanner(System.in);
-        System.out.println("2+3="+5);
-        int price;
-        int amount1 = 10, amount2 = 20;
-        price = in.nextInt();
-        System.out.println("100 - "+ price+" = " + (100-price));
-        System.out.println("amount1+amount2=" + (amount1 + amount2));
-        final int const_vatiable = 666; //const variable
-        System.out.println("the variable is: "+const_vatiable);
-        in.close();
+
+        //testing import 
+        Hello.main(args);
+
+        //testing input
+        // System.out.println("2+3="+5);
+        // int price;
+        // int amount1 = 10, amount2 = 20;
+        // price = in.nextInt();
+        // System.out.println("100 - "+ price+" = " + (100-price));
+        // System.out.println("amount1+amount2=" + (amount1 + amount2));
+        // final int const_vatiable = 666; //const variable
+        // System.out.println("the variable is: "+const_vatiable);
 
         //floating number
-        System.out.println(10/3);
-        System.out.println(10.0/3);
-        System.out.println(10/3.0);
-        System.out.println(10.0/3.0);
+        // System.out.println(10/3);
+        // System.out.println(10.0/3);
+        // System.out.println(10/3.0);
+        // System.out.println(10.0/3.0);
+
+        //the test of formatting print
+        test_print.main(args);
+
+        //testing of if-else, while and for loops
+        //two ways of computing factorial
+        //int n = in.nextInt();
+        int n = 15;
+        int i = 1;
+        int factorial = 1;
+        while (i <= n){
+            factorial = factorial * i;
+            i++;
+        }
+        System.out.println("the answer of "+n+"! is :"+factorial);
+        factorial = 1;
+        for (i = 1; i<=n; i++){
+            factorial = factorial * i;
+        }
+        System.out.println("the answer of "+n+"! is :"+factorial);
+        System.out.println("end of testing the loops");
+
+        //test of calculating gcd: greatest common divisor
+        int a = 12;
+        int b = 21;
+        int temp1, temp2;
+        temp1 = a; temp2 = b;
+        int ans = 1;
+        if (a<b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        while (a!=b){
+            while (a>b){
+                a = a-b;
+            }
+            int new_b = a;
+            a = b;
+            b = new_b;
+        }
+        ans = a;
+        System.out.printf("the gcd of %d and %d is: %d\n", temp1, temp2, ans);
+
+        //testing the life cycle of variables in java
+        int outside_bracket = 10;
+        if (true){
+            int inside_bracket = 15;
+            System.out.println("the inside_bracket is: "+inside_bracket);
+        }
+        System.out.println("the outside_bracket is: "+outside_bracket);
+        //System.out.println("the inside_bracket is: "+inside_bracket);
+        //this sentence is invalid because the variable will only exist in the {}
+
+        //test of array
+        
+
+        in.close();
     }
 }
